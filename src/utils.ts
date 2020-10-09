5afd81f35af55f0ada256daf87b5ca770047eacb
Eduardo San Martin Morote <posva13@gmail.com>
2020-10-09 10:02:28 +0200

    chore: only dev warn

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 export function assert(condition: boolean, message: string) {
-  if (!condition) {
+  // @ts-ignore
+  if (process.env.NODE_ENV !== 'production' && !condition) {
     // useful when testing
     // console.warn(`[vue-promised] ${message}`)
     throw new Error(`[vue-compose-promise] ${message}`)
